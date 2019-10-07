f9aa21554a6d7d669974c9292502027709d017a0
Michael Mardahl <iphase@iphase.dk>
2019-10-07 13:22:34 +0200

    Create enable-AADConnectTempPasswordSync.ps1
    Enables a preview feature of AAD Connect to sync the temporary password set by on-prem AD when "change password on next logon" is set on a user object.
    
    https://docs.microsoft.com/en-us/azure/active-directory/hybrid/how-to-connect-password-hash-synchronization#public-preview-of-synchronizing-temporary-passwords-and-force-password-on-next-logon

---
+++ enable-AADConnectTempPasswordSync.ps1
@@ -0,0 +1,19 @@
+#Requires -RunAsAdministrator
+
+# Script to enable "change password on first logon" sync to Azure AD
+# @michael_mardahl on twitter
+
+$AADConnector = Get-ADSyncConnector | Where-Object ListName -eq "Windows Azure Active Directory (Microsoft)"
+if ($AADConnector.count -eq 2) {
+
+    Write-Host "Enabling ForcePasswordResetOnLogonFeature on connector $($AADConnector.Name)." -ForegroundColor Green
+    Set-ADSyncAADCompanyFeature -ConnectorName $AADConnector.Name -ForcePasswordResetOnLogonFeature $true
+
+} else {
+
+    Write-Host "None OR more than one suitable connector found, run this command manually to determine which connector to use:" -ForegroundColor Yellow
+    Write-Host '(Get-ADSyncConnector | Where-Object ListName -eq "Windows Azure Active Directory (Microsoft)").Name'
+    Write-Host 'Then fill in the connector name you wish to use in this comand:' -ForegroundColor Yellow
+    Write-Host 'Set-ADSyncAADCompanyFeature -ConnectorName  "<CONNECTOR NAME FROM PREVIOUS COMMAND>" -ForcePasswordResetOnLogonFeature $true'
+
+}
