6b80c3bf207166efc12f30a19cda0d039d5f8464
Michael Mardahl <iphase@iphase.dk>
2020-10-04 19:57:30 +0200

    Create Replace-UserAuthenticationPhoneMethod.ps1
    Create a PoC for inspiration on using Microsoft Graph SDK for authentication pre-population

---
+++ Replace-UserAuthenticationPhoneMethod.ps1
@@ -0,0 +1,51 @@
+<# 
+  .SYNOPSIS
+    Script to replace authentication details for basic mobile MFA.
+    
+  .DESCRIPTION
+    This script will connect to the Microsoft Graph using the official PowerShell SDK and set/replace a specific users Mobile authentication details for MFA.
+    This is just some example code for PoC.
+    
+   .NOTES
+    Using Device Code login might not work with PowerShell ISE, so please execute from regular PS session.
+    Requires that the user authenticating the device code login has Authentication Administrator rights in AAD.
+    At the time of writing this, non-interactive login does not support replacing the authentication phone method.
+    
+    Autor: Michael Mardahl / github.com/Mardahl / @michael_mardahl
+   
+   .LICENSING
+    MIT standard license applies, please credit me if you learned something from this.
+
+#>
+#requires -module microsoft.graph
+#requires -Version 6
+
+#region declarations
+
+$targetUser = "myUser@myDomain.com"
+$targetUSerMobile = "+00 00000000" #Must be in this format, rememebr the space between country code and phone
+$graphApiVersion = 'beta' #Required MSGraphSettings as long as authentication is still in beta for the Graph API.
+$ErrorActionPreference = 'Stop'
+
+#endregion declarations
+
+#region execute
+
+#Connect to the desired graph endpoint (SLOW!)
+Select-MgProfile -Name $graphApiVersion
+
+#Connect to the Microsoft Graph using Device code interactive flow
+Connect-Graph -Scopes "User.Read.All", "UserAuthenticationMethod.ReadWrite.All"
+
+#Obtain user object from graph
+$user = get-mguser -UserId $targetUser
+$currentSettings = Get-MgUserAuthenticationPhoneMethod -UserId $user.Id
+
+#Check for any current phone and replace it.
+if ($currentSettings.PhoneNumber) {
+    Remove-MgUserAuthenticationPhoneMethod -UserId $user.Id -PhoneAuthenticationMethodId $currentSettings.Id
+}
+New-MgUserAuthenticationPhoneMethod -UserId $user.Id -PhoneNumber $targetUSerMobile -PhoneType "mobile"
+
+Disconnect-Graph #closing graph session
+#endregion execute
