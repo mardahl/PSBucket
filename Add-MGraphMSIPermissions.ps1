7e66702ccc8f5932562773464da7a0020065dbfd
Michael Mardahl <iphase@iphase.dk>
2022-09-21 13:26:52 +0200

    Update Add-MGraphMSIPermissions.ps1

--- Add-MGraphMSIPermissions.ps1
+++ Add-MGraphMSIPermissions.ps1
@@ -3,18 +3,21 @@
 Quick and dirty script by Michael Mardahl (github.com/mardahl)
 
 A PS script to be executed from Azure Cloud Shell - granting multiple Microsoft Graph permissions to a Managed Identity
+
+FYI: You can replace the Id in $AppId with other App Id's if you need to grant permission like Machine.Isolate for Microsoft Defender for Endpoint App.
 #>
 
-$MGraphAppId = "00000003-0000-0000-c000-000000000000"
-$DisplayNameOfMSI="AA-MFA-Prepopulate"
-$Permissions = @("user.read.all","reports.read.all","groupmember.read.all","UserAuthenticationMethod.ReadWrite.All")
+$AppId = "00000003-0000-0000-c000-000000000000" #default is the MS Graph AppId
+$DisplayNameOfMSI="myAppManagedIdentity"
+$Permissions = @("user.read.all","reports.read.all","groupmember.read.all","UserAuthenticationMethod.ReadWrite.All") #You can enter a single value here if you like.
 
 Connect-AzureAD
-$GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$MGraphAppId'"
+$ServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$AppId'"
 $MSI = (Get-AzureADServicePrincipal -Filter "displayName eq '$DisplayNameOfMSI'")
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 7
 
+#Setting permissions one at a 
 foreach ($PermissionName in $Permissions) {
-    $AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
-    New-AzureAdServiceAppRoleAssignment -ObjectId $MSI.ObjectId -PrincipalId $MSI.ObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole.Id
+    $AppRole = $ServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
+    New-AzureAdServiceAppRoleAssignment -ObjectId $MSI.ObjectId -PrincipalId $MSI.ObjectId -ResourceId $ServicePrincipal.ObjectId -Id $AppRole.Id
 }
