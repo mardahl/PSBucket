4c41f85a884d0a7ae061574affe1693aa4bb7c05
Michael Mardahl <iphase@iphase.dk>
2022-11-02 20:08:51 +0100

    Update Add-MGraphMSIPermissions.ps1
    Made MSI permissions script better

--- Add-MGraphMSIPermissions.ps1
+++ Add-MGraphMSIPermissions.ps1
@@ -4,20 +4,53 @@ Quick and dirty script by Michael Mardahl (github.com/mardahl)
 
 A PS script to be executed from Azure Cloud Shell - granting multiple Microsoft Graph permissions to a Managed Identity
 
-FYI: You can replace the Id in $AppId with other App Id's if you need to grant permission like Machine.Isolate for Microsoft Defender for Endpoint App.
+This version of the script defaults to clearing out any previous permissions!
+
 #>
 
+#region declarations
+
+#Application Id of the resource you want to grant permissions to (e.g. Microsoft Graph)
 $AppId = "00000003-0000-0000-c000-000000000000" #default is the MS Graph AppId
-$DisplayNameOfMSI="myAppManagedIdentity"
-$Permissions = @("user.read.all","reports.read.all","groupmember.read.all","UserAuthenticationMethod.ReadWrite.All") #You can enter a single value here if you like.
 
+#Display name of your managed identity
+$DisplayNameOfMSI="myCoolManagedIdentityName"
+
+#permissions to be granted (in powershell array format)
+$Permissions = @("group.readwrite.all","user.read.all") #You can enter a single value here if you like.
+
+#Clear existing permissions?
+$clearExistingPermissions = $true
+
+#endregion declarations
+
+#region execute
+
+#connect to Azure AD Powershell
 Connect-AzureAD
-$ServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$AppId'"
-$MSI = (Get-AzureADServicePrincipal -Filter "displayName eq '$DisplayNameOfMSI'")
-Start-Sleep -Seconds 7
 
-#Setting permissions one at a 
-foreach ($PermissionName in $Permissions) {
-    $AppRole = $ServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
-    New-AzureAdServiceAppRoleAssignment -ObjectId $MSI.ObjectId -PrincipalId $MSI.ObjectId -ResourceId $ServicePrincipal.ObjectId -Id $AppRole.Id
+# Get the Managed Identity Service Principal using displayName
+$MSI = (Get-AzureADServicePrincipal -Filter "displayName eq '$DisplayNameOfMSI'")
+start-sleep -seconds 3
+
+#Remove existing permissions if $clearExistingPermissions is $true
+if($clearExistingPermissions) {
+    # Get all application permissions for the managed identity service principal
+    $MSIApplicationPermissions = Get-AzureADServiceAppRoleAssignedTo -ObjectId $MSI.ObjectId -All $true | Where-Object { $_.PrincipalType -eq "ServicePrincipal" }
+
+    # Remove all application permissions for the managed identity service principal
+    $MSIApplicationPermissions | ForEach-Object {
+        Remove-AzureADServiceAppRoleAssignment -ObjectId $_.PrincipalId -AppRoleAssignmentId $_.objectId
+    }
 }
+
+#Get service principal for the application that we are granting permissions to
+$ResourceServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$AppId'"
+
+#Setting permissions one at a time
+foreach ($PermissionName in $Permissions) {
+    $AppRole = $ResourceServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
+    New-AzureAdServiceAppRoleAssignment -ObjectId $MSI.ObjectId -PrincipalId $MSI.ObjectId -ResourceId $ResourceServicePrincipal.ObjectId -Id $AppRole.Id
+}
+
+#endregion execute
